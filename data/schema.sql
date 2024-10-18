e99f67ee1fa0c93d2e6a44407861acea0dad55bb
Mark Burggraf <markb@mantisbible.com>
2024-10-17 18:10:27 -0700

    updated schema

--- data/schema.sql
+++ data/schema.sql
@@ -21,6 +21,40 @@ ALTER SCHEMA "public" OWNER TO "pg_database_owner";
 COMMENT ON SCHEMA "public" IS 'standard public schema';
 
 
+
+CREATE OR REPLACE FUNCTION "public"."get_org_role"("org_id" "uuid") RETURNS "text"
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    AS $$
+DECLARE
+    role TEXT;
+BEGIN
+    SELECT user_role INTO role
+    FROM orgs_users
+    WHERE orgid = org_id AND userid = auth.uid();
+
+    RETURN role;
+END;
+$$;
+
+
+ALTER FUNCTION "public"."get_org_role"("org_id" "uuid") OWNER TO "postgres";
+
+
+CREATE OR REPLACE FUNCTION "public"."is_backup_running"() RETURNS boolean
+    LANGUAGE "plpgsql"
+    AS $$
+BEGIN
+  RETURN EXISTS (
+    SELECT 1 
+    FROM pg_ls_dir('.')
+    WHERE pg_ls_dir = 'backup_label'
+  );
+END;
+$$;
+
+
+ALTER FUNCTION "public"."is_backup_running"() OWNER TO "postgres";
+
 SET default_tablespace = '';
 
 SET default_table_access_method = "heap";
@@ -33,18 +67,71 @@ CREATE TABLE IF NOT EXISTS "public"."contacts" (
     "lastname" "text",
     "email" "text",
     "phone" "text",
-    "userid" "uuid" DEFAULT "auth"."uid"() NOT NULL,
     "authid" "uuid",
     "contact_type" "text",
-    "notes" "text"
+    "notes" "text",
+    "orgid" "uuid" NOT NULL
 );
 
 
 ALTER TABLE "public"."contacts" OWNER TO "postgres";
 
 
+COMMENT ON TABLE "public"."contacts" IS 'List of Contacts (people)';
+
+
+
+CREATE TABLE IF NOT EXISTS "public"."orgs" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "title" "text" NOT NULL,
+    "metadata" "jsonb"
+);
+
+
+ALTER TABLE "public"."orgs" OWNER TO "postgres";
+
+
+COMMENT ON TABLE "public"."orgs" IS 'Organizations';
+
+
+
+CREATE TABLE IF NOT EXISTS "public"."orgs_users" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "orgid" "uuid" NOT NULL,
+    "userid" "uuid" NOT NULL,
+    "user_role" "text" NOT NULL
+);
+
+
+ALTER TABLE "public"."orgs_users" OWNER TO "postgres";
+
+
+COMMENT ON TABLE "public"."orgs_users" IS 'Users belong to Orgs';
+
+
+
 ALTER TABLE ONLY "public"."contacts"
     ADD CONSTRAINT "contacts_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."orgs"
+    ADD CONSTRAINT "orgs_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."orgs_users"
+    ADD CONSTRAINT "orgs_users_pkey" PRIMARY KEY ("id");
+
+
+
+CREATE INDEX "orgs_users_orgid_idx" ON "public"."orgs_users" USING "btree" ("orgid");
+
+
+
+CREATE INDEX "orgs_users_userid_idx" ON "public"."orgs_users" USING "btree" ("userid");
 
 
 
@@ -54,14 +141,40 @@ ALTER TABLE ONLY "public"."contacts"
 
 
 ALTER TABLE ONLY "public"."contacts"
-    ADD CONSTRAINT "contacts_userid_fkey" FOREIGN KEY ("userid") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+    ADD CONSTRAINT "contacts_orgid_fkey" FOREIGN KEY ("orgid") REFERENCES "public"."orgs"("id");
+
+
+
+ALTER TABLE ONLY "public"."orgs_users"
+    ADD CONSTRAINT "orgs_users_orgid_fkey" FOREIGN KEY ("orgid") REFERENCES "public"."orgs"("id");
+
+
+
+ALTER TABLE ONLY "public"."orgs_users"
+    ADD CONSTRAINT "orgs_users_userid_fkey" FOREIGN KEY ("userid") REFERENCES "auth"."users"("id");
+
+
+
+CREATE POLICY "User must belong to org" ON "public"."contacts" TO "authenticated" USING ((( SELECT "public"."get_org_role"("contacts"."orgid") AS "get_org_role") IS NOT NULL)) WITH CHECK ((( SELECT "public"."get_org_role"("contacts"."orgid") AS "get_org_role") IS NOT NULL));
 
 
 
 ALTER TABLE "public"."contacts" ENABLE ROW LEVEL SECURITY;
 
 
-CREATE POLICY "users control their own contacts" ON "public"."contacts" USING (("userid" = "auth"."uid"())) WITH CHECK (("userid" = "auth"."uid"()));
+ALTER TABLE "public"."orgs" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."orgs_users" ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY "users can view orgs they belong to" ON "public"."orgs" FOR SELECT USING (("id" IN ( SELECT "orgs_users"."orgid"
+   FROM "public"."orgs_users"
+  WHERE ("orgs_users"."userid" = ( SELECT "auth"."uid"() AS "uid")))));
+
+
+
+CREATE POLICY "users can view their own records" ON "public"."orgs_users" FOR SELECT USING (("userid" = "auth"."uid"()));
 
 
 
@@ -72,9 +185,33 @@ GRANT USAGE ON SCHEMA "public" TO "service_role";
 
 
 
+GRANT ALL ON FUNCTION "public"."get_org_role"("org_id" "uuid") TO "anon";
+GRANT ALL ON FUNCTION "public"."get_org_role"("org_id" "uuid") TO "authenticated";
+GRANT ALL ON FUNCTION "public"."get_org_role"("org_id" "uuid") TO "service_role";
+
+
+
+GRANT ALL ON FUNCTION "public"."is_backup_running"() TO "anon";
+GRANT ALL ON FUNCTION "public"."is_backup_running"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."is_backup_running"() TO "service_role";
+
+
+
 GRANT ALL ON TABLE "public"."contacts" TO "anon";
 GRANT ALL ON TABLE "public"."contacts" TO "authenticated";
 GRANT ALL ON TABLE "public"."contacts" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."orgs" TO "anon";
+GRANT ALL ON TABLE "public"."orgs" TO "authenticated";
+GRANT ALL ON TABLE "public"."orgs" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."orgs_users" TO "anon";
+GRANT ALL ON TABLE "public"."orgs_users" TO "authenticated";
+GRANT ALL ON TABLE "public"."orgs_users" TO "service_role";
 
 
 
